78bd687ddc292fb35c5a68148b76a269ca4003f1
year <yetao@newhope.cn>
2025-07-22 00:00:24 +0800

    任务2

--- task2/shape_3.go
+++ task2/shape_3.go
@@ -37,7 +37,7 @@ func (c Circle) Perimeter() float64 {
 /*
 第一题
 定义一个 Shape 接口，包含 Area() 和 Perimeter() 两个方法。然后创建 Rectangle 和 Circle 结构体，
-实现 Shape 接口。在主函数中，创建这两个结构体的实例，并调用它们的 Area() 和 Perimeter() 方法。
+实现 Shape 接口。在主函数中，创建这两个结构体的实例，并调用它们的 Area() 和 Perimeter()方法。
 */
 func main() {
 	var rect Rectangle = Rectangle{10, 5}
