871310b9c97024134734ba6716c98df6bf597503
year <yetao@newhope.cn>
2025-07-28 21:52:12 +0800

    rebuild

--- task3/SqlxMapping_2.go
+++ task3/SqlxMapping_2.go
@@ -1,19 +1,19 @@
-//package main
-//
-//import "fmt"
-//
-//type Book struct {
-//	ID     uint
-//	title  string
-//	author string
-//	price  float64
-//}
-//
-//func main() {
-//	var book []Book
-//	err := DB.Select(&book, "select * from books where price>50")
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//	fmt.Println(book)
-//}
+package main
+
+import "fmt"
+
+type Book struct {
+	ID     uint
+	title  string
+	author string
+	price  float64
+}
+
+func main() {
+	var book []Book
+	err := DB.Select(&book, "select * from books where price>50")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(book)
+}
